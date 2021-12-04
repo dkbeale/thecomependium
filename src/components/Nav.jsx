b9abf7c9ac61d3b23113ffec888094a542a65175
Dan <danielkbeale@gmail.com>
2021-12-04 19:31:56 +0000

    changed height of navbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -25,37 +25,3 @@ const Navigation = () => {
 
 export default Navigation;
 
-{
-  /* <Navbar expand="md" fixed="top" id="nav_bar" >
-    //   <Container id="nav_container">
-    //     <Navbar.Brand id="nav_brand">
-    //       <img
-    //         alt="site icon"
-    //         src={d20icon}
-    //         width="30"
-    //         height="30"
-    //         className="d-inline-block align-top"
-    //       />{" "}
-    //       The Compendium
-    //     </Navbar.Brand>
-    //     <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    //     <Navbar.Collapse id="basic-navbar-nav">
-    //       <Nav className="me-auto">
-    //         <LinkContainer to="/" className="nav_links">
-    //           <Nav.Link className="nav_links">Home</Nav.Link>
-    //         </LinkContainer>
-    //         <LinkContainer to="/spells">
-    //           <Nav.Link className="nav_links">Spells</Nav.Link>
-    //         </LinkContainer>
-    //         <LinkContainer to="/races">
-    //           <Nav.Link className="nav_links">Races</Nav.Link>
-    //         </LinkContainer>
-    //         <LinkContainer to="/classes">
-    //           <Nav.Link className="nav_links">Classes</Nav.Link>
-    //         </LinkContainer>
-    //       </Nav>
-    //     </Navbar.Collapse>
-    //   </Container>
-    // </Navbar>
- */
-}
